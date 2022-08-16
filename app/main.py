6b031a824b4eca7768c7eee85cecd01dcfc9f9f1
seungukkim <gogobattle@naver.com>
2022-08-16 16:16:20 +0900

    update

--- app/main.py
+++ app/main.py
@@ -203,19 +203,18 @@ def recommend():
         }
 
         return responseBody
-
     else:
         responseBody = {
-        "version": "2.0",
-        "template": {
-            "outputs": [
-                {
-                    "simpleText": {
-                        "text": "배고프다"
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "simpleText": {
+                            "text": "배고프다"
+                        }
                     }
-                }
-            ]
-        }
+                ]
+            }
         }
 
         return responseBody
