47e627d9de79e9ef23f024abbf1defa619b79116
seungukkim <gogobattle@naver.com>
2022-08-25 14:52:16 +0900

    update

--- app/main.py
+++ app/main.py
@@ -555,60 +555,9 @@ def recommen2d():
                   "messageText" : "샌드위치 더보기"
                 }
               ]
-            },
-            {
-              "header": {
-                "title": "커피"
-              },
-              "items": [
-                {
-                  "title": "아메리카노",
-                  "description": "1,800원",
-                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_05.jpg"
-                },
-                {
-                  "title": "카페라떼",
-                  "description": "2,000원",
-                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_06.jpg"
-                },
-                {
-                  "title": "카페모카",
-                  "description": "2,500원",
-                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_07.jpg"
-                },
-                {
-                  "title": "소이라떼",
-                  "description": "2,200원",
-                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_08.jpg"
-                }
-              ],
-              "buttons": [
-                {
-                  "label": "더보기",
-                  "action": "message",
-                  "messageText" : "커피 더보기"
-                }
-              ]
             }
           ]
         }
-      }
-    ],
-    "quickReplies": [
-      {
-        "messageText": "인기 메뉴",
-        "action": "message",
-        "label": "인기 메뉴"
-      },
-      {
-        "messageText": "최근 주문",
-        "action": "message",
-        "label": "최근 주문"
-      },
-      {
-        "messageText": "장바구니",
-        "action": "message",
-        "label": "장바구니"
       }
     ]
   }
