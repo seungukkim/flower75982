e134c4794d2eeccdcb3ceabf1098922858fb4b55
seungukkim <gogobattle@naver.com>
2022-08-25 15:52:19 +0900

    update

--- app/main.py
+++ app/main.py
@@ -521,21 +521,19 @@ def recommen2d():
       {
         "listCard": {
           "header": {
-            "title": "챗봇 관리자센터를 소개합니다."
+            "title": "장학금 추가 출력"
           },
           "items": [
             {
-              "title": "챗봇 관리자센터",
-              "description": "새로운 AI의 내일과 일상의 변화",
-              "imageUrl": "http://k.kakaocdn.net/dn/APR96/btqqH7zLanY/kD5mIPX7TdD2NAxgP29cC0/1x1.jpg",
+              "title": list1[5][2:-139],
+              "imageUrl": list1[5][-75:-2],
               "link": {
                 "web": "https://namu.wiki/w/%EB%9D%BC%EC%9D%B4%EC%96%B8(%EC%B9%B4%EC%B9%B4%EC%98%A4%ED%94%84%EB%A0%8C%EC%A6%88)"
               }
             },
             {
-              "title": "챗봇 관리자센터",
-              "description": "카카오톡 채널 챗봇 만들기",
-              "imageUrl": "http://k.kakaocdn.net/dn/N4Epz/btqqHCfF5II/a3kMRckYml1NLPEo7nqTmK/1x1.jpg",
+              "title": list1[6][2:-139],
+              "imageUrl": list1[6][-75:-2],
               "action": "block",
               "blockId": "62654c249ac8ed78441532de",
               "extra": {
@@ -544,9 +542,8 @@ def recommen2d():
               }
             },
             {
-              "title": "Kakao i Voice Service",
-              "description": "보이스봇 / KVS 제휴 신청하기",
-              "imageUrl": "http://k.kakaocdn.net/dn/bE8AKO/btqqFHI6vDQ/mWZGNbLIOlTv3oVF1gzXKK/1x1.jpg",
+              "title": list1[7][2:-139],
+              "imageUrl": list1[7][-75:-2],
               "action": "message",
               "messageText": "Kakao i Voice Service",
               "extra": {
